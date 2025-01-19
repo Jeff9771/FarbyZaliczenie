f55a3ccce1c13287a95ac3f1653684990b1c4e8c
Jeff <Jeff@DESKTOP-B578K2E>
2025-01-19 19:27:59 +0100

    Dodaj pliki projektów.

---
+++ FunnyWeb/Controllers/Data/ApplicationDBContext.cs
@@ -0,0 +1,23 @@
+﻿using FunnyWeb.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace FunnyWeb.Controllers.Data
+{
+    public class ApplicationDBContext : DbContext
+    {
+        public ApplicationDBContext(DbContextOptions<ApplicationDBContext> options) : base(options)
+        {
+        }
+
+        public DbSet<Category> Categories { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<Category>().HasData(
+                new Category { Id = 1, Name = "Vallejo Metal", PaintNumber = 77723, Type = "Metal" },
+                new Category { Id = 2, Name = "Vallejo Game", PaintNumber = 77323, Type = "Metal" },
+                new Category { Id = 3, Name = "Citadel Shade", PaintNumber = 52762, Type = "Metal" }
+            );
+        }
+    }
+}
---
+++ FunnyWeb/Models/Category.cs
@@ -0,0 +1,28 @@
+﻿using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
+
+namespace FunnyWeb.Models
+{
+    public class Category
+    {
+        [Key]
+        public int Id { get; set; }
+
+        [Required]
+        [MaxLength(30)]
+        [DisplayName("Nazwa Farby")]
+        public string Name { get; set; }
+
+        [Range(1,100000, ErrorMessage = "Numer farby powinien być w granicy 1-100000")]
+        [DisplayName("Numer Farby")]
+        public int PaintNumber { get; set; }
+
+
+
+        [MaxLength(30)]
+        [DisplayName("Typ Farby")]
+        public string Type { get; set; }
+
+
+    }
+}
